cb8c549f8e5595502274e36f848fa2220a3e7153
Ales Krot <ales.krot@gmail.com>
2021-05-24 20:10:27 +0300

    add: function in exercise-1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,85 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    
+    if (monthNumber < 1 || monthNumber > 12){
+        return nil;
+    }
+    
+    NSDateFormatter *formate = [[NSDateFormatter alloc] init];
+    
+    NSArray *monthNames = [formate standaloneMonthSymbols];
+    NSString *monthName = [monthNames objectAtIndex:(monthNumber - 1)];
+
+    return monthName;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    
+    NSString *dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = dateFormat;
+    
+    NSDate *dateFromString = [dateFormatter dateFromString:date];
+    
+    [dateFormatter setDateFormat:@"dd"];
+    NSString *stringFromDate = [dateFormatter stringFromDate:dateFromString];
+    
+    int number = [stringFromDate intValue];
+    
+    return number;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    dateFormatter.locale = [[NSLocale alloc]initWithLocaleIdentifier:@"ru_RU"];
+    [dateFormatter setDateFormat:@"E"];
+    
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    
+    NSCalendar *cal = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+
+    cal.firstWeekday = 2;
+
+    NSDate *now = [NSDate date];
+    NSDate *startOfTheWeek;
+    NSDate *endOfWeek;
+    NSTimeInterval interval;
+    [cal rangeOfUnit:NSCalendarUnitWeekOfYear
+           startDate:&startOfTheWeek
+            interval:&interval
+             forDate:now];
+
+    endOfWeek = [startOfTheWeek dateByAddingTimeInterval:interval - 1];
+
+    [cal rangeOfUnit:NSCalendarUnitDay
+           startDate:&endOfWeek
+            interval:NULL
+             forDate:endOfWeek];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    formatter.timeStyle = NSDateFormatterShortStyle;
+    
+    if ([date timeIntervalSinceDate:startOfTheWeek] > 0 &&
+        [date timeIntervalSinceDate:endOfWeek] < 0){
+        return YES;
+    }
+    
+    else{
+        return NO;
+    }
 }
 
 @end
